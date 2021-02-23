5214335cdd1c32646bf365bc90a0e1de27c7ed83
trainee3 <trainee3@wefttechnologies.com>
2021-02-23 17:11:38 +0530

    Initial

---
+++ WebApplication1/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class User
+    {
+        public int Id{ get; set;}
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public String Position { get; set; }
+
+
+
+
+
+    }
+}
